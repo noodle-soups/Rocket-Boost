402f926080e11dc253b815d3d8f3ba9f7961cceb
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2025-01-06 19:44:08 +0000

    commit

---
+++ Assets/Scripts/FloatingRock.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FloatingRock : MonoBehaviour
+{
+
+    [Header("Rotation")]
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float xRotation;
+    [SerializeField] private float yRotation;
+    [SerializeField] private float zRotation;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 _rotationVector = new Vector3(xRotation, yRotation, zRotation);
+        transform.Rotate(_rotationVector * rotationSpeed * Time.fixedDeltaTime);
+
+    }
+}
